96a597c514db82de0efea91d26a3b657d993c0c5
fenris <anatolijv236@gmail.com>
2020-10-18 16:49:56 +0300

    adaptive

--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,16 @@ $(document).ready(() => {
         prevArrow: '<button type="button" class="slick-prev"><svg width="66" height="66" viewBox="0 0 66 66" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="33" cy="33" r="30" fill="#666666" stroke="white" stroke-width="6"/><path fill-rule="evenodd" clip-rule="evenodd" d="M30.4529 32.5001L36.2265 38.2736L35.1069 39.3932L28.2137 32.5001L35.1069 25.6069L36.2265 26.7265L30.4529 32.5001Z" fill="white"/></svg></button>',
         nextArrow: '<button type="button" class="slick-next"><svg width="66" height="66" viewBox="0 0 66 66" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="33" cy="33" r="30" fill="#666666" stroke="white" stroke-width="6"/><path fill-rule="evenodd" clip-rule="evenodd" d="M30.4529 32.5001L36.2265 38.2736L35.1069 39.3932L28.2137 32.5001L35.1069 25.6069L36.2265 26.7265L30.4529 32.5001Z" fill="white"/></svg></button>',
         slidesToShow: 2,
-        autoplay: false,
+        autoplay: true,
         responsive: [
             {
                 breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 640,
                 settings: {
                     slidesToShow: 1,
                     arrows: false
@@ -32,15 +38,14 @@ $(document).ready(() => {
         nextArrow: '<button type="button" class="slick-next"><svg width="66" height="66" viewBox="0 0 66 66" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="33" cy="33" r="30" fill="#666666" stroke="white" stroke-width="6"/><path fill-rule="evenodd" clip-rule="evenodd" d="M30.4529 32.5001L36.2265 38.2736L35.1069 39.3932L28.2137 32.5001L35.1069 25.6069L36.2265 26.7265L30.4529 32.5001Z" fill="white"/></svg></button>',
         slidesToShow: 3,
         autoplay: false,
-        // responsive: [
-        //     {
-        //         breakpoint: 767,
-        //         settings: {
-        //             slidesToShow: 1,
-        //             arrows: false
-        //         }
-        //     }
-        // ]
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                }
+            }
+        ]
     });
 
     /**
@@ -82,4 +87,12 @@ $(document).ready(() => {
         .on('click', function () {
             $(this).get(0).setSelectionRange(4, 4);
         });
+
+    /**
+     * HAMBURGER
+     */
+    $('.menu-toggle-inner').on('click', function (e) {
+        $('.adaptive-menu-toggle').toggleClass('adaptive-menu-toggle--open');
+        $('.headerNav_adaptive').toggleClass('header_opened');
+    });
 });
